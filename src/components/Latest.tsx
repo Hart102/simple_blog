c51085e3560081af0d5632689b95030d5b927dc4
Hart102 <hartongit102@gmail.com>
2023-09-05 19:42:38 -0400

    created a related news component

--- src/components/Latest.tsx
+++ src/components/Latest.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
 import Title from "@/components/_child/Title"
 import NewsCard from "@/components/_child/NewsCard"
-
 import { Data } from '@/pages/Api/Data';
 import Error from './_child/Error';
 
 
-const Latest: React.FC = () => {
+const Latest = () => {
 
     if(!Data) return <Error />
 
@@ -16,7 +14,7 @@ const Latest: React.FC = () => {
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-20">
 
                 {Data[1].map((post, index) => (
-                    <NewsCard key={index} data={post} gridCols='md:grid-cols-1' titleSize='text-1xl' className='w-100 h-64'/>
+                    <NewsCard key={index} data={post} gridCols='md:grid-cols-1' titleSize='text-1xl' className='w-100 h-56'/>
                 ))}
                 
             </div>
